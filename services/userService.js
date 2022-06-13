508c99c57c4120a3d1d8db51da58c7ba6f019978
mishabenq <mixa200221@gmail.com>
2022-06-14 00:04:28 +0300

    Added API for user

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,14 @@ class UserService {
 
     // TODO: Implement methods to work with user
 
+    getAllUsers() {
+        const item = UserRepository.getAll();
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
@@ -11,6 +19,32 @@ class UserService {
         }
         return item;
     }
+
+    update(id, dataToUpdate) {
+        const item = UserRepository.update(id, dataToUpdate);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    create(data) {
+        const item = UserRepository.create(data);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    delete(id) {
+        const item = UserRepository.delete(id);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+
 }
 
 module.exports = new UserService();
