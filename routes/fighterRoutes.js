6ac3d0b92c933b5ce0bf4364b0706062ef3c9655
mishabenq <mixa200221@gmail.com>
2022-06-14 15:45:58 +0300

    added fighter routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -7,4 +7,59 @@ const router = Router();
 
 // TODO: Implement route controllers for fighter
 
+router.post('/create', (req, res, next) => {
+    try {
+        data = FighterService.create(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/delete', (req, res, next) => {
+    try {
+        data = FighterService.delete(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/getAll', (req, res, next) => {
+    try {
+        data = FighterService.getAll();
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/search', (req, res, next) => {
+    try {
+        data = FighterService.search(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/update', (req, res, next) => {
+    try {
+        data = FighterService.update(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
 module.exports = router;
