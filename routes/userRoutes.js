09d371f490bd8ec9bb3dbf9ebd8e3667fb313e43
mishabenq <mixa200221@gmail.com>
2022-06-14 15:22:44 +0300

    added user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,59 @@ const router = Router();
 
 // TODO: Implement route controllers for user
 
+router.post('/create', (req, res, next) => {
+    try {
+        data = UserService.create(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/delete', (req, res, next) => {
+    try {
+        data = UserService.delete(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/getAll', (req, res, next) => {
+    try {
+        data = UserService.getAll();
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/search', (req, res, next) => {
+    try {
+        data = UserService.search(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.post('/update', (req, res, next) => {
+    try {
+        data = UserService.update(req);
+        res.data = data;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
 module.exports = router;
